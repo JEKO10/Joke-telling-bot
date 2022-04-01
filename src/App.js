8be82ab81f7b4d1393975d50d25d6898cdcb4e26
Aleksa Bubanja <aleksa3b@gmail.com>
2022-04-01 17:32:07 +0200

    fetching jokes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <></>;
+  const [joke, setJoke] = useState();
+
+  const fetchJoke = async () => {
+    const response = await fetch(
+      "https://v2.jokeapi.dev/joke/Programming,Pun?type=single"
+    );
+    const jokeData = await response.json();
+    setJoke(jokeData);
+    console.log(jokeData);
+  };
+
+  useEffect(() => {
+    fetchJoke();
+  }, []);
+
+  return (
+    <>
+      <h1>{joke.joke}</h1>
+      <button onClick={fetchJoke}>Tell me a joke</button>
+    </>
+  );
 }
 
 export default App;
